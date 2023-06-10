31c480232188a6b32c2b74eaa0db8732166385fc
yousefh409 <yousefh409@gmail.com>
2023-06-09 22:40:55 -0700

    itinerary

--- src/pages/itinerary.jsx
+++ src/pages/itinerary.jsx
@@ -1,10 +1,41 @@
 import React, { useState } from "react";
+import "../styles/Itinerary.css"
 
 export const Itinerary = () => {
 
     return (
-        <div>
-            itinerary
+        <div className="itineraryWrapper">
+            <h4>Below are some more of the specifics of the trip:</h4>
+          
+            <h1>Travel</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+            <div className="break" />
+            <h1>Accomdations</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+       
+            <div className="break" />
+            <h1>Day-by-Day Schedule</h1>
+            <ul>
+                <li className="scheduleListItem">Day 1</li>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                <li className="scheduleListItem">Day 2</li>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                <li className="scheduleListItem">Day 3</li>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                <li className="scheduleListItem">Day 4</li>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                <li className="scheduleListItem">Day 5</li>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                <li className="scheduleListItem">Day 6</li>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                <li className="scheduleListItem">Day 7</li>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                <li className="scheduleListItem">Day 8</li>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit
+
+            </ul>
+       
         </div>
     );
 };
