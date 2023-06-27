8d018c969b549edacd220715754c642f4d60b341
yousefh409 <yousefh409@gmail.com>
2023-06-26 18:17:41 -0700

    home page updated

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,74 +1,45 @@
 import React, { useState } from "react";
 import Banner from "../components/Banner";
-import "../styles/Umrah.scss"
+import "../styles/Home.scss"
 import elbakri from '../images/shaykh-el-bakri.png';
 import darElSalam from '../images/dar-el-salam.jpg';
 import kaabah from '../images/kaabah.webp';
 import masjidElNabawy from '../images/masjidAlNabawi.jpg';
+import homeBannerImg from '../images/homeBanner.jpg'
 
 export const Home = () => {
     
     return (
-        <div className="umrahWrapper">
-            <Banner />
+        <div className="homeWrapper">
+            <div className="homeBanner">
+                <div className="homeBannerText">
+                    <p>Bringing together the Bay Area Muslim student communities together</p>
+                </div>
+                <img className="homeBannerImg" src={homeBannerImg} alt="Home Banner Image" />
+            </div>
             <div className="break" />
-
-            <div>
+           <div>
                 <h1>About</h1>
-                <div className="aboutWrapper">
-                    <div  className="card">
-                        <div className="card-body">
-                            This is a once in a lifetime trip, that will bring together the Berkeley and Stanford muslim student populations. Together, we will embark on an 11-night trip to the blessed cities of Mecca and Madinah. 
-                            There, we will not only complete the holy rites of Umrah, but also visit and learn about many of the historic sites that we read about in our Islamic books.
-                            We hope to be able to raise funds to make this trip as affordable as possible, so please consider <a href="/support">donating today</a> and spreading the word!
-                        </div>
-                        <a style={{margin: "10px"}} href='/register' className='btn btn-primary'>
-                            Register Today
-                        </a>
-                    </div>
+                <div className="homeWrapperContent">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum fermentum ut diam imperdiet sollicitudin. Praesent pellentesque justo odio, vitae accumsan purus auctor non. Etiam eu metus vel nibh venenatis blandit vitae quis orci. Vivamus lacinia iaculis quam in imperdiet. Nam eu consectetur nisl. Nunc sit amet efficitur enim, eget tincidunt diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum fermentum ut diam imperdiet sollicitudin. Praesent pellentesque justo odio, vitae accumsan purus auctor non. Etiam eu metus vel nibh venenatis blandit vitae quis orci. Vivamus lacinia iaculis quam in imperdiet. Nam eu consectetur nisl. Nunc sit amet efficitur enim, eget tincidunt diam.
                 </div>
-            </div>      
-            {/* <div className="break" /> */}
-            
-            {window.innerWidth > 768? <div className="umrahImageWrapper">
-                <img className="umrahImage islamicBorder" src={kaabah} alt="Picture of hotel room" />
-                <img className="umrahImage islamicBorder" src={masjidElNabawy} alt="Picture of hotel room" />
-            </div>: <div />}
-            <div>
-                <h1>Donations</h1>
-                <div className="aboutWrapper">
-                    <div  className="card">
-                        <div className="card-body">
-                            We want to make this trip as affordable as possible (or even free if we raise enough funds) for the students attending.
-                            To do this, we need your help (and wallet). Your donations will go to taking students on an amazing trip to visit some of Islams most famous sites, inspiring them for the rest of their lives!
-                            Any amount is appreciated, and even just sharing this with your network would bring us closer to our goals.
-                        </div>
-                        <a style={{margin: "10px"}} href='/support' className='btn btn-primary'>
-                            Donate Today
-                        </a>
-                    </div>
+           </div>
+           <div className="breakSmall" />
+           <a style={{width: "100%"}} href='/support' className='btn btn-primary'>
+                Donate Today
+            </a>
+          
+           <div className="break" />
+           <div>
+                <h1>Umrah</h1>
+                <div className="homeWrapperContent">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum fermentum ut diam imperdiet sollicitudin. Praesent pellentesque justo odio, vitae accumsan purus auctor non. Etiam eu metus vel nibh venenatis blandit vitae quis orci. Vivamus lacinia iaculis quam in imperdiet. Nam eu consectetur nisl. Nunc sit amet efficitur enim, eget tincidunt diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum fermentum ut diam imperdiet sollicitudin. Praesent pellentesque justo odio, vitae accumsan purus auctor non. Etiam eu metus vel nibh venenatis blandit vitae quis orci. Vivamus lacinia iaculis quam in imperdiet. Nam eu consectetur nisl. Nunc sit amet efficitur enim, eget tincidunt diam.
                 </div>
-            </div>
-            <div className="break" />
-            <div>
-                <h1>The Hosts</h1>
-                <div className="hostsWrapper">
-                    <div className="card" style={{width: "23rem", margin: "7px"}}>
-                        <img className="card-img-top" src={elbakri} alt="Card image cap" />
-                        <div className="card-body">
-                            <h5 className="card-title">Shaykh Alauddin El-Bakri</h5>
-                            <p className="card-text">Shaykh Alauddin El-Bakri is a founding member of several non-profit organizations that serve the Muslim community such as (IMAN) Inner-City Muslim Action Network, My Dean, Support Life Foundation and Tawasaw.org. Shaykh El-Bakri is currently the resident scholar and Khateeb at West Valley Muslim association. Shaykh El-Bakri will be leading and guiding us throughout the trip.</p>
-                        </div>
-                    </div>
-                    <div className="card" style={{width: "23rem", margin: "7px"}}>
-                        <img className="card-img-top" src={darElSalam} alt="Card image cap" />
-                        <div className="card-body">
-                            <h5 className="card-title">Dar El Salam</h5>
-                            <p className="card-text">Dar El Salam has been providing high-quality service for over 30 years, making them the top Umrah facilitator in North America. They have organized trips to the holy lands of Mecca and Madinah for over 100,000 people and counting! Dar El Salam will be organizing and managing the umrah trip.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+           </div>
+           <div className="breakSmall" />
+           <a style={{width: "100%"}} href='/umrah' className='btn btn-primary'>
+                Learn More
+            </a>
         </div>
     );
-};
+};                     
